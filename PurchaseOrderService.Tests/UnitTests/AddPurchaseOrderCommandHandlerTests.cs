c5370924b4b5919f10566d1f55a7bd8fd124281f
Peter255 <developer.peter.reda@gmail.com>
2025-01-12 01:21:01 +0200

    Add new tests and update project configurations
    Updated PurchaseOrderService.Tests.csproj to include FluentAssertions package and project references for Application and Domain projects. Removed UnitTests folder reference. Deleted UnitTest1.cs. Added AddPurchaseOrderCommandHandlerTests with two test methods. Added PurchaseOrderService.lutconfig for repository and build settings. Updated PurchaseOrderService.sln to include the new test project and configurations.

---
+++ PurchaseOrderService.Tests/UnitTests/AddPurchaseOrderCommandHandlerTests.cs
@@ -0,0 +1,53 @@
+﻿using Moq;
+using FluentAssertions;
+using PurchaseOrderService.Domain.Interfaces;
+using PurchaseOrderService.Application.Commands.CreatePurchaseOrder;
+using PurchaseOrderService.Domain.ValueObjects;
+
+
+namespace PurchaseOrderService.Tests.UnitTests
+{
+    public class AddPurchaseOrderCommandHandlerTests
+    {
+        [Fact]
+        public async Task Handle_Should_Add_PurchaseOrder()
+        {
+            // Arrange
+            var mockRepository = new Mock<IPurchaseOrderRepository>();
+            var handler = new CreatePurchaseOrderHandler(mockRepository.Object);
+            var command = new CreatePurchaseOrderCommand("PO123410101", DateTime.Now, 100.00m, new List<PurchaseOrderItemDto>
+                {
+                    new PurchaseOrderItemDto("GC1234574575", new Money(50.00m)),
+                    new PurchaseOrderItemDto("GC1234674111", new Money(150.00m)),
+                });
+
+            // Act
+            var result = await handler.Handle(command, default);
+
+            // Assert
+            result.Should().Be(1); // Assuming the result is an integer representing the number of records added
+            mockRepository.Verify(r => r.AddAsync(It.IsAny<Domain.Entities.PurchaseOrder>()), Times.Once);
+        }
+
+        [Fact]
+        public async Task Handle_Should_Throw_Exception_When_TotalPrice_Is_Negative()
+        {
+            // Arrange
+            var mockRepository = new Mock<IPurchaseOrderRepository>();
+            var handler = new CreatePurchaseOrderHandler(mockRepository.Object);
+            var command = new CreatePurchaseOrderCommand("PO12345", DateTime.Now, -50.00m, new List<PurchaseOrderItemDto>
+                {
+                    new PurchaseOrderItemDto("GC1234574585", new Money(50.00m)),
+                    new PurchaseOrderItemDto("GC1234678787", new Money(150.00m)),
+                });
+
+            // Act
+            Func<Task> action = async () => await handler.Handle(command, default);
+
+            // Assert
+            await action.Should().ThrowAsync<ArgumentException>()
+                .WithMessage("Total price cannot be negative.");
+        }
+    }
+}
+
